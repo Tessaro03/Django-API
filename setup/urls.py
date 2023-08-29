79093f962d5da4cefacb82e5569b146ba0c53d43
Tessaro03 <gabrieltessarofavareto@gmail.com>
2023-08-29 14:47:19 -0300

    att 0.6

--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno,ListaAlunosMatriculados
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 router = routers.DefaultRouter()
 router.register('alunos',AlunosViewSet,basename='Alunos')
@@ -14,4 +17,4 @@ urlpatterns = [
     path('',include(router.urls)),
     path('aluno/<int:pk>/matriculas/',ListaMatriculasAluno.as_view()),
     path('curso/<int:pk>/matriculas/',ListaAlunosMatriculados.as_view())
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
